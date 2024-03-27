7cb0b38e1a0820d9a44ca34acf24b3851fa7a6bc
Karlos Warney <karloswarney@gmail.com>
2024-03-27 16:43:05 -0300

    ADD - Método para gerar resposta.

--- src/main/java/com/warney/application/domain/model/Champion.java
+++ src/main/java/com/warney/application/domain/model/Champion.java
@@ -1,4 +1,13 @@
 package com.warney.application.domain.model;
 
 public record Champion(Long id, String name, String role, String lore, String imageUrl) {
+
+    public String generateContextByQuestion(String question){
+        return """
+                Pergunta: %s
+                Nome do Campeão: %s
+                Função: %s
+                Lore (História): %s
+                """.formatted(question, this.name, this.role, this.lore);
+    }
 }
