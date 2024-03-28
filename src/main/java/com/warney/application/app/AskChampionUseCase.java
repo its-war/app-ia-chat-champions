cf7c866952dd574b7818e3ece90101841ae72e4c
Karlos Warney <karloswarney@gmail.com>
2024-03-28 16:52:05 -0300

    ALTER - Estrutura de geração de prompt para as IAs foi implementada.

--- src/main/java/com/warney/application/app/AskChampionUseCase.java
+++ src/main/java/com/warney/application/app/AskChampionUseCase.java
@@ -3,15 +3,21 @@ package com.warney.application.app;
 import com.warney.application.domain.exception.ChampionNotFoundException;
 import com.warney.application.domain.model.Champion;
 import com.warney.application.domain.ports.ChampionsRepository;
+import com.warney.application.domain.ports.GenerativeAiService;
 
-public record AskChampionUseCase(ChampionsRepository repository) {
+public record AskChampionUseCase(ChampionsRepository repository, GenerativeAiService genAiApi) {
 
     public String askChampion(Long id, String question){
         Champion champion = repository.findById(id)
                 .orElseThrow(() -> new ChampionNotFoundException(id));
 
-        // TODO: evoluir a lógica de negocio para considerar a integração com IAs generativas.
+        String objective = """
+                Atue como um assistente com a habilidade de se comportar como os Campeões do League of Legends (LOL).
+                Responda perguntas incorporando a personalidade e estilo de um determinado campeão.
+                Segue a pergunta, o campeão e sua respectiva lore (história):
+                """;
+        String context = champion.generateContextByQuestion(question);
 
-        return champion.generateContextByQuestion(question);
+        return genAiApi.generativeContent(objective, context);
     }
 }
